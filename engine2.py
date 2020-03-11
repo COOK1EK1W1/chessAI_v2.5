d5ce73e86676e4f30d934a9868a04533ca70ba3d
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2020-03-11 18:16:02 +0000

    combining old version

--- engine2.py
+++ engine2.py
@@ -1,7 +1,11 @@
-import time, random, chess
+import time
+import random
+import chess
 
 # turn true == white
-values = {"p":1, "n":3, "b":3, "r":5, "q":9, "P":-1, "N":-3, "B":-3, "R":-5, "Q":-9, "k":0, "K":0}
+values = {"p": 1, "n": 3, "b": 3, "r": 5, "q": 9, "P": -
+          1, "N": -3, "B": -3, "R": -5, "Q": -9, "k": 0, "K": 0}
+
 
 class ChessAi:
     def __init__(self, lookahead):
@@ -33,17 +37,21 @@ class ChessAi:
         for move in moves:
             X, Y = int(columns.index(str(move)[0])), int(str(move)[1])
             if X == x and Y == y and not flipped:
-                marker_location.append((int(columns.index(str(move)[2])), -int(str(move)[3]) + 8))
+                marker_location.append(
+                    (int(columns.index(str(move)[2])), -int(str(move)[3]) + 8))
             if -X + 7 == x and Y == y and flipped:
-                marker_location.append((-int(columns.index(str(move)[2])) + 7, int(str(move)[3]) - 1))
+                marker_location.append(
+                    (-int(columns.index(str(move)[2])) + 7, int(str(move)[3]) - 1))
         return marker_location
 
     def coor_move(self, X, Y, x, y, flipped, promotion=""):
         columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
         if not flipped:
-            self.board.push(chess.Move.from_uci(str(columns[X] + str(Y) + str(columns[x] + str(y) + promotion))))
+            self.board.push(chess.Move.from_uci(
+                str(columns[X] + str(Y) + str(columns[x] + str(y) + promotion))))
         if flipped:
-            self.board.push(chess.Move.from_uci(str(columns[-X + 7] + str(-Y + 9) + str(columns[-x + 7] + str(-y + 9) + promotion))))
+            self.board.push(chess.Move.from_uci(str(
+                columns[-X + 7] + str(-Y + 9) + str(columns[-x + 7] + str(-y + 9) + promotion))))
 
     def makemove(self, player, flipped):
         start = time.time()
@@ -54,8 +62,10 @@ class ChessAi:
         print(str([str(move) for move in self.board.legal_moves]))
         print("I" + "-" * len(list(self.board.legal_moves)) + "I", end="\r")
         for i, move in enumerate(self.board.legal_moves):
-            time_remaining = "  " + str(round((time.time() - start) * (len(list(self.board.legal_moves)) / (i + 1)) - (time.time() - start), 3)) + "s"
-            print("I" + "#" * i + "-" * (len(list(self.board.legal_moves)) - i) + "I" + time_remaining, end="\r")
+            time_remaining = "  " + str(round((time.time() - start) * (
+                len(list(self.board.legal_moves)) / (i + 1)) - (time.time() - start), 3)) + "s"
+            print("I" + "#" * i + "-" * (len(list(self.board.legal_moves)
+                                             ) - i) + "I" + time_remaining, end="\r")
             score = 0
             tempboard = self.board.copy()
             tempboard.push(move)
@@ -70,7 +80,8 @@ class ChessAi:
                     score += 0.5
                 else:
                     score -= 0.5
-            score += minimax(tempboard, depth, -float("inf"), float("inf"), not player)
+            score += minimax(tempboard, depth, -float("inf"),
+                             float("inf"), not player)
             scores.append(score)
         print(scores, sum(scores) / len(scores))
         if player:
@@ -80,7 +91,7 @@ class ChessAi:
         return list(self.board.legal_moves)[random.choice(x)]
 
 
-def evalboard(source):## higher score for white
+def evalboard(source):  # higher score for white
     pieces = source.piece_map()
     pieces = list(pieces.values())
     peicetotal = 0
@@ -94,9 +105,10 @@ def evalboard(source):## higher score for white
             peicetotal -= values[str(piece)]
     return peicetotal
 
+
 def minimax(board, depth, alpha, beta, maximizingPlayer):
     if depth == 0 or board.is_game_over():
-       return evalboard(board)
+        return evalboard(board)
     if maximizingPlayer:
         maxEval = -float("inf")
         for move in board.legal_moves:
